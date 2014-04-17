78662c5d7c55bf0d8803e80f69e795feb1d779b5
bjtuweb-12281038 <12281038@bjtu.edu.cn>
2014-04-17 09:29:30 +0800

    login

---
+++ login/check.php
@@ -0,0 +1,30 @@
+﻿<?php
+error_reporting(E_ALL ^ E_NOTICE);
+$link = mysql_connect("localhost","root","root","0");
+mysql_select_db("users",$link);
+$username = $_POST['username'];
+$pwd = $_POST['pwd'];
+$exec="select * from user";
+$result = mysql_query($exec);
+$n = 0;
+while($row = mysql_fetch_array($result))
+{
+	if($row['username'] == $username)
+	{
+		if($row['pwd'] == $pwd)
+		{
+		    echo "<script>alert('欢迎进入！')</script>";		
+			header("Location:1.php");
+			$n=1;
+			break;
+		}
+	}
+}
+
+if($n==0)
+{
+   echo "<script>alert('用户名不存在或密码错误！')</script>";
+   header("Location:index.php");
+}
+mysql_close($link);
+?>
---
+++ login/index.php
@@ -0,0 +1,18 @@
+﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">  
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />  
+<title>用户登录</title>  
+<body>  
+<fieldset>    
+<legend>用户登录页面</legend>  
+<form  method="post">    
+<p>用户名：      
+  <input type="text" name=username></p>    
+<p>密&nbsp;&nbsp;&nbsp;&nbsp;码：     
+  <input type="password" name=pwd></p>     
+<p><input type="button" value="注&nbsp;&nbsp;&nbsp;册" name="register" onclick="location='register.php'">      
+<input type="button" value="登&nbsp;&nbsp;&nbsp;录" name="login" onclick="location='check.php'"></p>  
+</form>  
+</fieldset>  
+</body>  
+</html>
